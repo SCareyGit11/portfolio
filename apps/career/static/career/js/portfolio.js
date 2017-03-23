669e80ba6ff85a7f56b5483f15f6bc979dfd9bd5
Sean Carey <seanv74672@gmail.com>
2017-03-22 23:47:54 -0700

    initial commit

---
+++ apps/career/static/career/js/portfolio.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.img-thumbnail').hover(
+		// hover in, show the alt-img by making it the primary src and save original src as the new alt
+		function(){
+				console.log("The alt source is " + $(this).attr('data-alt-src'));
+				var alt = $(this).attr('data-alt-src');
+				var temp = $(this).attr('src');
+				$(this).attr('src', alt);
+				$(this).attr('data-alt-src', temp);
+		}, 
+		// hover out, show original image by making it src again and alt is back to alt
+		function(){
+				console.log("The alt source is " + $(this).attr('data-alt-src'));
+				var original = $(this).attr('data-alt-src');
+				var temp = $(this).attr('src');
+				$(this).attr('src', original);
+				$(this).attr('data-alt-src', temp);
+		})
+})
