752685ed887fa9480dfe3c9e6592c438bf964543
Sean Carey <seanv74672@gmail.com>
2017-05-10 16:58:42 -0700

    xs

--- apps/career/static/career/js/portfolio.js
+++ apps/career/static/career/js/portfolio.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('.img-thumbnail').hover(
 		// hover in, show the alt-img by making it the primary src and save original src as the new alt
 		function(){
-				console.log("The alt source is " + $(this).attr('data-alt-src'));
+				
 				var alt = $(this).attr('data-alt-src');
 				var temp = $(this).attr('src');
 				$(this).attr('src', alt);
@@ -10,7 +10,7 @@ $(document).ready(function(){
 		}, 
 		// hover out, show original image by making it src again and alt is back to alt
 		function(){
-				console.log("The alt source is " + $(this).attr('data-alt-src'));
+				
 				var original = $(this).attr('data-alt-src');
 				var temp = $(this).attr('src');
 				$(this).attr('src', original);
